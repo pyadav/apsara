b9901cb9723947408969e3232230d31711a773e8
Praveen Yadav <pyadav9678@gmail.com>
2020-07-21 11:45:06 +0530

    🏈Add react hook into eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,34 @@
 module.exports = {
     // Specifies the ESLint parser
     parser: "@typescript-eslint/parser",
-    settings: { react: { version: "999.999.999" } },
-    parserOptions: {
-        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-        sourceType: "module", // Allows for the use of imports
-        ecmaFeatures: {
-            jsx: true, // Allows for the parsing of JSX
-        },
-    },
     extends: [
+        "eslint:recommended",
         "plugin:react/recommended",
         "plugin:@typescript-eslint/recommended",
         "prettier/@typescript-eslint",
         "plugin:prettier/recommended",
     ],
+    plugins: ["react", "@typescript-eslint", "prettier", "react-hooks"],
+    parserOptions: {
+        ecmaVersion: 2020,
+        ecmaFeatures: {
+            jsx: true,
+        },
+        sourceType: "module",
+    },
+    env: {
+        browser: true,
+        node: true,
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/no-var-requires": 0,
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": [2],
     },
 };
